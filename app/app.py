7919d546d34ff259505129d89ed60dd55acb005b
BM-Unnop <122899012+Unnop-Thamtiengtham@users.noreply.github.com>
2023-10-12 05:48:38 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 # Function to check if a number is prime
 def is_prime(number):
     if number <= 1:
-        return 'false'
+        return 'False'
     if number <= 3:
-        return 'true'
+        return 'True'
     if number % 2 == 0 or number % 3 == 0:
-        return 'false'
+        return 'False'
     i = 5
     while i * i <= number:
         if number % i == 0 or number % (i + 2) == 0:
-            return'false'
+            return'False'
         i += 6
-    return 'true'
+    return 'True'
 
 @app.route('/is_prime/<int:number>', methods=['GET'])
 def check_prime(number):
